a7528703a2eb190dac45de3f8494f71c8446a6ee
Muhammadayyub <muhammadayyubraufjonov7102@gmail.com>
2026-02-19 15:48:42 +0500

    tamam

--- app/apis/expense_api.py
+++ app/apis/expense_api.py
@@ -5,7 +5,7 @@ from app.services import expense_service
 
 
 
-router = APIRouter
+router = APIRouter()
 
 
 @router.post('/expenses', response_model=ExpenseResponse)
@@ -14,6 +14,17 @@ def create_exp(data: ExpenseCreate, current_user:CurrentUserDep, db: DBDep):
 
 
 
-@router.post('/expenses', response_model=list[ExpenseResponse])
-def create_exp(current_user:CurrentUserDep, db: DBDep):
+@router.get('/expenses', response_model=list[ExpenseResponse])
+def read_exp(current_user:CurrentUserDep, db: DBDep):
     return expense_service.handle_get_all( current_user, db)
+
+
+@router.put('/expenses/{expense_id}', response_class=ExpenseResponse)
+def update_exp(expense_id: int,data: ExpenseCreate, current_user:CurrentUserDep, db: DBDep):
+    return expense_service.handle_update(expense_id, data, current_user, db)
+
+
+@router.delete('/expenses/{expense_id}', response_class=ExpenseResponse)
+def delete_exp(expense_id: int, current_user:CurrentUserDep, db: DBDep):
+    return expense_service.handle_delete(expense_id, current_user, db)
+
--- app/services/expense_service.py
+++ app/services/expense_service.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from app.db import Expense
 from app.schemas.expense_schemas import ExpenseCreate
 from app.schemas.auth_schemas import LoggedInUser
+from fastapi import HTTPException
+
 
 def handle_exp_create(data: ExpenseCreate, current_user:LoggedInUser, db:Session):
     expense = Expense(**data.model_dump(), user_id=current_user.user_id)
@@ -15,3 +17,25 @@ def handle_exp_create(data: ExpenseCreate, current_user:LoggedInUser, db:Session
 
 def handle_get_all(current_user:LoggedInUser, db:Session):
     return db.execute(select(Expense).where(Expense.user_id==current_user.user_id)).scalars().all()
+
+
+def handle_update(expense_id: int, data: ExpenseCreate, current_user:LoggedInUser, db:Session): 
+    expense = db.execute(select(Expense).where(Expense.id==expense_id, Expense.user_id==current_user.user_id)).scalar_one_or_none()
+    if not expense:
+        raise HTTPException(status_code=404, detail="Expense is not found!")
+        
+    for key, value in data.model_dump().item():
+        setattr(expense, key, value)
+        
+    db.add()
+    db.refresh(expense)
+    return expense
+
+def handle_delete(expense_id: int, data: ExpenseCreate, current_user:LoggedInUser, db:Session): 
+    expense = db.execute(select(Expense).where(Expense.id==expense_id, Expense.user_id==current_user.user_id)).scalar_one_or_none()
+    if not expense:
+        raise HTTPException(status_code=404, detail="Expense is not found!")
+           
+    db.delete(expense)
+    db.commit()
+    return "detail: Expense is deleted!"
